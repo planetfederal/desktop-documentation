25086ef672340281727e2572efbc7f2bf9824395
Alexandre Neto <senhor.neto@gmail.com>
2017-03-02 12:10:12 +0000

    Adds Version argument to choose Core documentation version depending on Boundless desktop version

--- offline_docs/prepare_offline.sh
+++ offline_docs/prepare_offline.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#
+# Accepted parameters:
+#
+# Version: Boundless Desktop's version (e.g. 1.0)
 
 set -e
 
+Version=$1
 # Clean things up
 mkdir -p tmp
 cd tmp
@@ -10,22 +15,33 @@ mkdir output
 
 # Getting QGIS Core Documentation
 
-if [ -d "QGIS-Documentation" ]; then 
-  cd QGIS-Documentation;
-  git pull origin manual_en_2.14;
-else 
-  git clone https://github.com/qgis/QGIS-Documentation.git
-  cd QGIS-Documentation;
-  git fetch origin manual_en_2.14;
+if [ ! -d "QGIS-Documentation" ]; then
+  git clone https://github.com/qgis/QGIS-Documentation.git;
 fi
-git checkout manual_en_2.14
-if  [ -d "qgisdocs_virtualenv" ]; then
+cd QGIS-Documentation;
+
+rm -rf output;
+
+case "$Version" in
+	"1.0")
+		git fetch origin manual_en_2.14;
+		git checkout manual_en_2.14;
+		git merge origin/manual_en_2.14;
+		;;
+	*)
+		git checkout master;
+		git pull origin master;
+		;;
+esac
+
+if [ -d "qgisdocs_virtualenv" ]; then
    source qgisdocs_virtualenv/bin/activate;
 else
    virtualenv qgisdocs_virtualenv;
    source qgisdocs_virtualenv/bin/activate;
    pip install -r REQUIREMENTS.txt;
 fi
+
 make html
 deactivate
 rsync -uthvr --delete output/ ../output/qgis_core_docs
@@ -33,14 +49,13 @@ cd ..
 
 # Getting Desktop documentation
 
-if [ -d "desktop-documentation" ]; then 
-  cd desktop-documentation;
-  git pull origin master;
-  cd docs;
-else 
+if [ ! -d "desktop-documentation" ]; then 
   git clone --recursive https://github.com/boundlessgeo/desktop-documentation.git;
-  cd desktop-documentation/docs;
-fi
+fi  
+
+cd desktop-documentation/docs;
+git pull origin master;
+
 make html
 rsync -uthvr --delete build/ ../../output/desktop_doc
 
@@ -48,13 +63,12 @@ cd ../..
 
 # Appending Plugins Documentation
 
-if [ -d "qgis-plugins-documentation" ]; then 
-  cd qgis-plugins-documentation;
-  git pull origin master;
-else 
+if [ ! -d "qgis-plugins-documentation" ]; then 
   git clone https://github.com/boundlessgeo/qgis-plugins-documentation.git;
-  cd qgis-plugins-documentation;
-fi
+fi 
+  
+cd qgis-plugins-documentation;
+git pull origin master;
 
 paver fetch
 paver builddocs
@@ -64,7 +78,7 @@ rsync -uthvr --delete output/ ../output/plugins
 
 cd ..
 
-#Cleaning up links
+#Redirecting links
 REGXHTML="output/desktop_doc/html/plugins/supported_plugins.html"
 if [[ "${OSTYPE}" =~ ^darwin ]]; then
   sed -i '' -E 's@\.\./\.\./plugins/([[:alnum:]]+)/@../../plugins/\1/index.html@g' ${REGXHTML}
@@ -75,24 +89,20 @@ fi
 
 # Getting Learning Center
 
-# if [ -d "connect-learning" ]; then 
-#   cd connect-learning;
-#   git pull origin master;
-# else 
-#   git clone --recursive https://github.com/boundlessgeo/connect-learning.git;
-#   cd connect-learning;
-# fi
-# cd desktop
-# make html-offline
-# #make get-datasets
-# #make get-videos
-# rsync -uthvr --delete build/ ../../output/desktop-learning
-# cd ../../..
+if [ ! -d "connect-learning" ]; then 
+   git clone --recursive https://github.com/boundlessgeo/connect-learning.git;
+fi
+
+cd connect-learning/desktop;
+git pull origin master;
+
+make html-offline
+#make get-videos
+rsync -uthvr --delete build/ ../../output/desktop-learning
+cd ../../..
 
 
-#Place Index Page
+#Put index Page in place
 cd ..
 rsync -uthvr _static/ tmp/output/_static
 cp index.html tmp/output
-
-
