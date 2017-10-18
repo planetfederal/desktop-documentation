6932103372787a8c2a1cd17a3f9c1a4cc2bedaff
Alexandre Neto <senhor.neto@gmail.com>
2017-10-18 16:32:41 +0100

    Updates offline docs script

--- offline_docs/prepare_offline.sh
+++ offline_docs/prepare_offline.sh
@@ -13,40 +13,6 @@ cd tmp
 rm -rf output 
 mkdir output
 
-# Getting QGIS Core Documentation
-
-if [ ! -d "QGIS-Documentation" ]; then
-  git clone https://github.com/qgis/QGIS-Documentation.git;
-fi
-cd QGIS-Documentation;
-
-rm -rf output;
-
-case "$Version" in
-	"1.0")
-		git fetch origin manual_en_2.14;
-		git checkout manual_en_2.14;
-		git merge origin/manual_en_2.14;
-		;;
-	*)
-		git checkout master;
-		git pull origin master;
-		;;
-esac
-
-if [ -d "qgisdocs_virtualenv" ]; then
-   source qgisdocs_virtualenv/bin/activate;
-else
-   virtualenv qgisdocs_virtualenv;
-   source qgisdocs_virtualenv/bin/activate;
-   pip install -r REQUIREMENTS.txt;
-fi
-
-make html
-deactivate
-rsync -uthvr --delete output/ ../output/qgis_core_docs
-cd ..
-
 # Getting Desktop documentation
 
 if [ ! -d "desktop-documentation" ]; then 
@@ -61,6 +27,11 @@ case "$Version" in
 		git checkout r1.0;
 		git merge origin/r1.0;
 		;;
+#	"1.1")
+#		git fetch origin r1.1;
+#		git checkout r1.1;
+#		git merge origin/r1.1;
+#		;;		
 	*)
 		git checkout master;
 		git pull origin master;
@@ -74,28 +45,28 @@ cd ../..
 
 # Appending Plugins Documentation
 
-if [ ! -d "qgis-plugins-documentation" ]; then 
-  git clone https://github.com/boundlessgeo/qgis-plugins-documentation.git;
-fi 
-  
-cd qgis-plugins-documentation;
-git pull origin master;
-
-paver fetch
-paver builddocs
-paver deployoffline
-
-rsync -uthvr --delete output/ ../output/plugins
-
-cd ..
-
-#Redirecting links
-REGXHTML="output/desktop_doc/html/plugins/supported_plugins.html"
-if [[ "${OSTYPE}" =~ ^darwin ]]; then
-  sed -i '' -E 's@\.\./\.\./plugins/([[:alnum:]]+)/@../../plugins/\1/index.html@g' ${REGXHTML}
-else
-  sed -ri "s@\.\./\.\./plugins/(\w*)/@../../plugins/\1/index.html@g" ${REGXHTML}
-fi
+#if [ ! -d "qgis-plugins-documentation" ]; then 
+#  git clone https://github.com/boundlessgeo/qgis-plugins-documentation.git;
+#fi 
+#  
+#cd qgis-plugins-documentation;
+#git pull origin master;
+#
+#paver fetch
+#paver builddocs
+#paver deployoffline
+#
+#rsync -uthvr --delete output/ ../output/plugins
+#
+#cd ..
+#
+##Redirecting links
+#REGXHTML="output/desktop_doc/html/plugins/supported_plugins.html"
+#if [[ "${OSTYPE}" =~ ^darwin ]]; then
+#  sed -i '' -E 's@\.\./\.\./plugins/([[:alnum:]]+)/@../../plugins/\1/index.html@g' ${REGXHTML}
+#else
+#  sed -ri "s@\.\./\.\./plugins/(\w*)/@../../plugins/\1/index.html@g" ${REGXHTML}
+#fi
 
 
 # Getting Learning Center
@@ -112,6 +83,46 @@ fi
 #rsync -uthvr --delete build/ ../../output/desktop-learning
 #cd ../..
 
+# Getting QGIS Core Documentation
+
+if [ ! -d "QGIS-Documentation" ]; then
+  git clone https://github.com/qgis/QGIS-Documentation.git;
+fi
+cd QGIS-Documentation;
+
+rm -rf output;
+
+case "$Version" in
+	"1.0")
+		git fetch origin manual_en_2.14;
+		git checkout manual_en_2.14;
+		git merge origin/manual_en_2.14;
+		;;
+	"1.1")
+	    git fetch origin release_2.18;
+		git checkout release_2.18;
+		git merge origin/release_2.18;
+		;;
+	*)
+		git checkout master;
+		git pull origin master;
+		;;
+esac
+
+echo "Setting up virtual enviornment..."
+
+if [ -d "qgisdocs_virtualenv" ]; then
+   rm -rf qgisdocs_virtualenv 
+fi
+
+virtualenv qgisdocs_virtualenv;
+source qgisdocs_virtualenv/bin/activate;
+pip install -r REQUIREMENTS.txt;
+
+make html
+deactivate
+rsync -uthvr --delete output/ ../output/qgis_core_docs
+cd ..
 
 #Put index Page in place
 cd ..
