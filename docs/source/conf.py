22c57c08784c50cdf39c6c2d59f6aac886adfe20
Alexandre Neto <senhor.neto@gmail.com>
2018-10-25 00:33:42 +0100

    Integrate plugins docs in desktop documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,7 @@ release = '2.0.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['**/README.rst','**/CONTRIBUTING.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
